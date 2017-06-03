039a68de73a708bc8c1030453964182e6c757d5d
Timo Wiren <timo.wiren@gmail.com>
2017-06-03 21:50:37 +0300

    vertex buffer, shader beginnings

--- source/gfxdevicevulkan.d
+++ source/gfxdevicevulkan.d
@@ -1,5 +1,6 @@
 import erupted;
 import std.stdio;
+import core.stdc.string;
 
 extern(System) VkBool32 MyDebugReportCallback(
     VkDebugReportFlagsEXT       flags,
@@ -130,6 +131,21 @@ class GfxDeviceVulkan
             attachments[ 0 ] = swapChainBuffers[ bufferIndex ].view;
             enforceVk( vkCreateFramebuffer( device, &frameBufferCreateInfo, null, &frameBuffers[ bufferIndex ] ) );
         }
+
+        const float s = 100;
+        quadVertices = new VertexPTC[ 4 ];
+        quadVertices[ 0 ] = VertexPTC( 0, 0, 0, 0, 0 );
+        quadVertices[ 1 ] = VertexPTC( s, 0, 0, 1, 0 );
+        quadVertices[ 2 ] = VertexPTC( s, s, 0, 1, 1 );
+        quadVertices[ 3 ] = VertexPTC( 0, s, 0, 0, 1 );
+
+        quadIndices = new Face[ 2 ];
+        quadIndices[ 0 ] = Face( 0, 1, 2 );
+        quadIndices[ 1 ] = Face( 2, 3, 0 );
+
+        vertexBuffer.generate( quadVertices, quadIndices, this );
+    
+        shader.load( "unlit_vert.spv", "unlit_frag.spv", device );
     }
 
     private void createDescriptorSetLayout()
@@ -873,6 +889,172 @@ class GfxDeviceVulkan
         enforceVk( vkCreateSemaphore( device, &renderCompleteSemaphoreCreateInfo, null, &renderCompleteSemaphore ) );
     }
 
+    void copyBuffer( VkBuffer source, ref VkBuffer destination, int bufferSize )
+    {
+        VkCommandBufferAllocateInfo cmdBufInfo;
+        cmdBufInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+        cmdBufInfo.commandPool = cmdPool;
+        cmdBufInfo.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+        cmdBufInfo.commandBufferCount = 1;
+
+        VkCommandBuffer copyCommandBuffer;
+        enforceVk( vkAllocateCommandBuffers( device, &cmdBufInfo, &copyCommandBuffer ) );
+
+        VkCommandBufferBeginInfo cmdBufferBeginInfo;
+        cmdBufferBeginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+        cmdBufferBeginInfo.pNext = null;
+
+        VkBufferCopy copyRegion;
+        copyRegion.size = bufferSize;
+
+        enforceVk( vkBeginCommandBuffer( copyCommandBuffer, &cmdBufferBeginInfo ) );
+
+        vkCmdCopyBuffer( copyCommandBuffer, source, destination, 1, &copyRegion );
+
+        enforceVk( vkEndCommandBuffer( copyCommandBuffer ) );
+
+        VkSubmitInfo copySubmitInfo;
+        copySubmitInfo.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO;
+        copySubmitInfo.commandBufferCount = 1;
+        copySubmitInfo.pCommandBuffers = &copyCommandBuffer;
+
+        enforceVk( vkQueueSubmit( graphicsQueue, 1, &copySubmitInfo, VK_NULL_HANDLE ) );
+        enforceVk( vkQueueWaitIdle( graphicsQueue ) );
+
+        vkFreeCommandBuffers( device, cmdBufInfo.commandPool, 1, &copyCommandBuffer );
+    }
+
+    void createBuffer( ref VkBuffer buffer, int bufferSize, ref VkDeviceMemory memory, VkBufferUsageFlags usageFlags, VkMemoryPropertyFlags memoryFlags, string debugName )
+    {
+        VkBufferCreateInfo bufferInfo;
+        bufferInfo.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
+        bufferInfo.size = bufferSize;
+        bufferInfo.usage = usageFlags;
+        enforceVk( vkCreateBuffer( device, &bufferInfo, null, &buffer ) );
+        //debug::SetObjectName( gDevice, (std::uint64_t)buffer, VK_DEBUG_REPORT_OBJECT_TYPE_BUFFER_EXT, debugName );
+
+        VkMemoryRequirements memReqs;
+        VkMemoryAllocateInfo memAlloc;
+        memAlloc.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
+
+        vkGetBufferMemoryRequirements( device, buffer, &memReqs );
+        memAlloc.allocationSize = memReqs.size;
+        getMemoryType( memReqs.memoryTypeBits, memoryFlags, &memAlloc.memoryTypeIndex );
+        enforceVk( vkAllocateMemory( device, &memAlloc, null, &memory ) );
+        enforceVk( vkBindBufferMemory( device, buffer, memory, 0 ) );
+        assert( buffer != VK_NULL_HANDLE, "buffer is null" );
+    }
+
+    struct SwapChainBuffer
+    {
+        VkImage image;
+        VkImageView view;
+    }
+
+    struct DepthStencil
+    {
+        VkImage image;
+        VkDeviceMemory mem;
+        VkImageView view;
+    }
+
+    struct VertexPTC
+    {
+        this( float aX, float aY, float aZ, float aU, float aV )
+        {
+            x = aX;
+            y = aY;
+            z = aZ;
+            u = aU;
+            v = aV;
+        }
+
+        float x = 0, y = 0, z = 0;
+        float u = 0, v = 0;
+        float r = 0, g = 0, b = 0, a = 0;
+    }
+
+    struct Face
+    {
+        this( ushort aA, ushort aB, ushort aC )
+        {
+            a = aA;
+            b = aB;
+            c = aC;
+        }
+
+        ushort a = 0, b = 0, c = 0;
+    }
+
+    struct Shader
+    {
+      void load( string vertexPath, string fragmentPath, VkDevice device )
+        {
+            // Vertex shader
+            {
+                auto file = File( vertexPath, "r" );
+
+                if (!file.isOpen())
+                {
+                    assert( false, "Could not open vertex shader file" );
+                }
+                
+                char[] vertexData = new char[ 100 ];
+
+                VkShaderModuleCreateInfo moduleCreateInfo;
+                moduleCreateInfo.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
+                moduleCreateInfo.pNext = null;
+                moduleCreateInfo.codeSize = vertexData.length;
+                moduleCreateInfo.pCode = cast(uint*)vertexData.ptr;
+                moduleCreateInfo.flags = 0;
+
+                VkShaderModule shaderModule;
+                enforceVk( vkCreateShaderModule( device, &moduleCreateInfo, null, &shaderModule ) );
+
+                vertexInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+                vertexInfo.stage = VK_SHADER_STAGE_VERTEX_BIT;
+                vertexInfo._module = shaderModule;
+                vertexInfo.pName = "main";
+                vertexInfo.pNext = null;
+                vertexInfo.flags = 0;
+                vertexInfo.pSpecializationInfo = null;
+            }
+
+            // Fragment shader
+            {
+                auto file = File( fragmentPath, "r" );
+                
+                if (!file.isOpen())
+                {
+                    assert( false, "Could not open fragment shader file" );
+                }
+
+                char[] fragmentData = new char[ 100 ];
+
+                VkShaderModuleCreateInfo moduleCreateInfo;
+                moduleCreateInfo.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
+                moduleCreateInfo.pNext = null;
+                moduleCreateInfo.codeSize = fragmentData.length;
+                moduleCreateInfo.pCode = cast(uint*)fragmentData.ptr;
+                moduleCreateInfo.flags = 0;
+
+                VkShaderModule shaderModule;
+                enforceVk( vkCreateShaderModule( device, &moduleCreateInfo, null, &shaderModule ) );
+
+                fragmentInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+                fragmentInfo.stage = VK_SHADER_STAGE_FRAGMENT_BIT;
+                fragmentInfo._module = shaderModule;
+                fragmentInfo.pName = "main";
+                fragmentInfo.pNext = null;
+                fragmentInfo.flags = 0;
+                fragmentInfo.pSpecializationInfo = null;
+            }
+        }
+      
+        VkPipelineShaderStageCreateInfo vertexInfo;
+        VkPipelineShaderStageCreateInfo fragmentInfo;
+    }
+
     VkSurfaceKHR surface;
     VkDevice device;
     VkInstance instance;
@@ -897,22 +1079,111 @@ class GfxDeviceVulkan
     VkDescriptorPool descriptorPool;
     VkPipelineLayout pipelineLayout;
     int queueNodeIndex;
-    uint currentBuffer;
-  
-    struct SwapChainBuffer
-    {
-        VkImage image;
-        VkImageView view;
-    }
-
+    uint currentBuffer;  
     SwapChainBuffer[] swapChainBuffers;
-
-    struct DepthStencil
+    DepthStencil depthStencil;
+    VertexPTC[] quadVertices;
+    Face[] quadIndices;
+    Shader shader;
+  
+    struct VertexBuffer
     {
-        VkImage image;
-        VkDeviceMemory mem;
-        VkImageView view;
+        void generate( VertexPTC[] vertices, Face[] indices, GfxDeviceVulkan gfxDevice )
+        {
+            struct StagingBuffer
+            {
+                VkDeviceMemory memory;
+                VkBuffer buffer;
+            }
+
+            struct StagingBuffers
+            {
+                StagingBuffer vertices;
+                StagingBuffer indices;
+            }
+
+            StagingBuffers stagingBuffers;
+            
+            int vertexBufferSize = cast(int)(vertices.length * VertexPTC.sizeof);
+
+            // Vertex buffer
+            gfxDevice.createBuffer( stagingBuffers.vertices.buffer, vertexBufferSize, stagingBuffers.vertices.memory, VK_BUFFER_USAGE_TRANSFER_SRC_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT, "staging vertex buffer" );
+
+            void* bufferData = null;
+            enforceVk( vkMapMemory( gfxDevice.device, stagingBuffers.vertices.memory, 0, vertexBufferSize, 0, &bufferData ) );
+
+            memcpy( bufferData, vertices.ptr, vertexBufferSize );
+            vkUnmapMemory( gfxDevice.device, stagingBuffers.vertices.memory );
+
+            gfxDevice.createBuffer( vertexBuffer, vertexBufferSize, vertexMem, VK_BUFFER_USAGE_VERTEX_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, "vertex buffer" );
+            assert( vertexBuffer != VK_NULL_HANDLE, "vertex buffer is null" );
+            gfxDevice.copyBuffer( stagingBuffers.vertices.buffer, vertexBuffer, vertexBufferSize );
+
+            vkDestroyBuffer( gfxDevice.device, stagingBuffers.vertices.buffer, null );
+            vkFreeMemory( gfxDevice.device, stagingBuffers.vertices.memory, null );
+
+            // Index buffer
+            int indexBufferSize = cast(int)(indices.length * Face.sizeof);
+
+            gfxDevice.createBuffer( stagingBuffers.indices.buffer, indexBufferSize, stagingBuffers.indices.memory, VK_BUFFER_USAGE_TRANSFER_SRC_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT, "staging index buffer" );
+
+            enforceVk( vkMapMemory( gfxDevice.device, stagingBuffers.indices.memory, 0, indexBufferSize, 0, &bufferData ) );
+
+            memcpy( bufferData, indices.ptr, indexBufferSize );
+            vkUnmapMemory( gfxDevice.device, stagingBuffers.indices.memory );
+
+            gfxDevice.createBuffer( indexBuffer, indexBufferSize, indexMem, VK_BUFFER_USAGE_INDEX_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, "index buffer" );
+            gfxDevice.copyBuffer( stagingBuffers.indices.buffer, indexBuffer, indexBufferSize );
+
+            vkDestroyBuffer( gfxDevice.device, stagingBuffers.indices.buffer, null );
+            vkFreeMemory( gfxDevice.device, stagingBuffers.indices.memory, null );
+
+            const int VERTEX_BUFFER_BIND_ID = 0;
+            const int POSITION_INDEX = 0;
+            const int TEXCOORD_INDEX = 1;
+            const int COLOR_INDEX = 2;
+            
+            bindingDescriptions = new VkVertexInputBindingDescription[ 1 ];
+            bindingDescriptions[ 0 ].binding = VERTEX_BUFFER_BIND_ID;
+            bindingDescriptions[ 0 ].stride = VertexPTC.sizeof;
+            bindingDescriptions[ 0 ].inputRate = VK_VERTEX_INPUT_RATE_VERTEX;
+
+            attributeDescriptions = new VkVertexInputAttributeDescription[ 3 ];
+
+            // Location 0 : Position
+            attributeDescriptions[ 0 ].binding = VERTEX_BUFFER_BIND_ID;
+            attributeDescriptions[ 0 ].location = POSITION_INDEX;
+            attributeDescriptions[ 0 ].format = VK_FORMAT_R32G32B32_SFLOAT;
+            attributeDescriptions[ 0 ].offset = 0;
+
+            // Location 1 : TexCoord
+            attributeDescriptions[ 1 ].binding = VERTEX_BUFFER_BIND_ID;
+            attributeDescriptions[ 1 ].location = TEXCOORD_INDEX;
+            attributeDescriptions[ 1 ].format = VK_FORMAT_R32G32_SFLOAT;
+            attributeDescriptions[ 1 ].offset = float.sizeof * 3;
+
+            // Location 2 : Color
+            attributeDescriptions[ 2 ].binding = VERTEX_BUFFER_BIND_ID;
+            attributeDescriptions[ 2 ].location = COLOR_INDEX;
+            attributeDescriptions[ 2 ].format = VK_FORMAT_R32G32B32A32_SFLOAT;
+            attributeDescriptions[ 2 ].offset = float.sizeof * 5;
+
+            inputState.sType = VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO;
+            inputState.pNext = null;
+            inputState.vertexBindingDescriptionCount = cast(uint32_t)bindingDescriptions.length;
+            inputState.pVertexBindingDescriptions = bindingDescriptions.ptr;
+            inputState.vertexAttributeDescriptionCount = cast(uint32_t)attributeDescriptions.length;
+            inputState.pVertexAttributeDescriptions = attributeDescriptions.ptr;
+        }
+
+        VkBuffer vertexBuffer;
+        VkDeviceMemory vertexMem;
+        VkPipelineVertexInputStateCreateInfo inputState;
+        VkBuffer indexBuffer;
+        VkDeviceMemory indexMem;
+        VkVertexInputBindingDescription[] bindingDescriptions;
+        VkVertexInputAttributeDescription[] attributeDescriptions;
     }
 
-    DepthStencil depthStencil;
+    VertexBuffer vertexBuffer;
 }

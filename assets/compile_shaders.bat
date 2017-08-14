c9f5d5a6c09dc9d4c96cb8d3b2aef39f54dc18e2
Timo Wiren <timo.wiren@gmail.com>
2017-08-14 20:51:09 +0300

    Update SDK to 1.0.57 and fix validation errors

--- assets/compile_shaders.bat
+++ assets/compile_shaders.bat
@@ -1,3 +1,3 @@
-\VulkanSDK\1.0.51.0\Bin\glslangValidator.exe -V shader.frag -o shader.frag.spv
-\VulkanSDK\1.0.51.0\Bin\glslangValidator.exe -V shader.vert -o shader.vert.spv
+\VulkanSDK\1.0.57.0\Bin\glslangValidator.exe -V shader.frag -o shader.frag.spv
+\VulkanSDK\1.0.57.0\Bin\glslangValidator.exe -V shader.vert -o shader.vert.spv
 pause

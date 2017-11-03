b8a8a0390a4e51525d69eb5781603ef0bc0add0b
Timo Wiren <timo.wiren@gmail.com>
2017-11-03 10:58:14 +0200

    Update SDK to 1.0.65.0

--- assets/compile_shaders.bat
+++ assets/compile_shaders.bat
@@ -1,3 +1,3 @@
-\VulkanSDK\1.0.61.0\Bin\glslangValidator -D -V -S vert -e main shader_vert.hlsl -o shader_vert_hlsl.spv
-\VulkanSDK\1.0.61.0\Bin\glslangValidator -D -V -S frag -e main shader_frag.hlsl -o shader_frag_hlsl.spv
+\VulkanSDK\1.0.65.0\Bin\glslangValidator -D -V -S vert -e main shader_vert.hlsl -o shader_vert_hlsl.spv
+\VulkanSDK\1.0.65.0\Bin\glslangValidator -D -V -S frag -e main shader_frag.hlsl -o shader_frag_hlsl.spv
 pause

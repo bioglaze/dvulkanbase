74d232216e6b7aefd14d8ecc1dc74a7398af33ee
Timo Wiren <timo.wiren@gmail.com>
2018-02-10 16:50:04 +0200

    Update SDK

--- assets/compile_shaders.sh
+++ assets/compile_shaders.sh
@@ -1,3 +1,3 @@
-~/Downloads/VulkanSDK/1.0.65.0/x86_64/bin/glslangValidator -D -V -S vert -e main shader_vert.hlsl -o shader_vert_hlsl.spv
-~/Downloads/VulkanSDK/1.0.65.0/x86_64/bin/glslangValidator -D -V -S frag -e main shader_frag.hlsl -o shader_frag_hlsl.spv
+$VULKAN_SDK/bin/glslangValidator -D -V -S vert -e main shader_vert.hlsl -o shader_vert_hlsl.spv
+$VULKAN_SDK/bin/glslangValidator -D -V -S frag -e main shader_frag.hlsl -o shader_frag_hlsl.spv
 

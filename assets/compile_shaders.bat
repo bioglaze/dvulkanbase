8098614e364a9f0089457eae3cd789d6750ed308
Timo Wiren <timo.wiren@gmail.com>
2018-10-13 16:33:11 +0300

    Use SDK environment variable

--- assets/compile_shaders.bat
+++ assets/compile_shaders.bat
@@ -1,3 +1,3 @@
-\VulkanSDK\1.0.85.0\Bin\glslangValidator -D -V -S vert -e main shader_vert.hlsl -o shader_vert_hlsl.spv
-\VulkanSDK\1.0.85.0\Bin\glslangValidator -D -V -S frag -e main shader_frag.hlsl -o shader_frag_hlsl.spv
+%VULKAN_SDK%\Bin\glslangValidator -D -V -S vert -e main shader_vert.hlsl -o shader_vert_hlsl.spv
+%VULKAN_SDK%\Bin\glslangValidator -D -V -S frag -e main shader_frag.hlsl -o shader_frag_hlsl.spv
 pause
